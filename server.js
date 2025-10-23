335c67258c91c1c44a895b189be574be0c88d800
controllerofpython <express.vanukuri@gmail.com>
2025-10-23 00:20:11 -0700

    Update server to use environment variable for PORT

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('.'));
 
 app.get('/search', async (req, res) => {
     try {
@@ -42,6 +43,10 @@ app.get('/search', async (req, res) => {
     }
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
